729e66bf93e5ba3e336b3a383a76cf9a69502398
LeMemaker <derpyg61@gmail.com>
2023-04-15 15:04:17 -0400

    lotta stuff

--- location.js
+++ location.js
@@ -1,3 +1,5 @@
+import browserEnv from 'browser-env';
+browserEnv(['navigator']);
 const VarHolder = {
     speed : 0.0
 };
@@ -5,12 +7,26 @@ const VarHolder = {
 const location = {
     
     isCalcMPH: true,
-    speed: new GeolocationCoordinates,
+    
+    currentPos: function(){
+        let currentLat = position.coords.latitude;
+        let currentLong = position.coords.longitude;
+    },
+
+    newPos:function(){
+        let newLat = position.coords.latitude;
+        let newLong = position.coords.longitude;
+    },
+
     saveMPH: function(){
+        navigator.geolocation.getCurrentPosition(location.currentPos);
+        setTimeout(() => {  navigator.geolocation.getCurrentPosition(location.newPos); }, 5000);
         
-        setTimeout(() => {  VarHolder.speed = location.speed.speed; }, 5000);
-            
-        
+        milesTravelled = Math.acos(Math.sin(currentLat)*Math.sin(newLat)+Math.cos(currentLat)*Math.cos(newLat)*Math.cos(currentLong - newLong)*6371);
+
+        MPH = milesTravelled / 5 / 5 * 360;
+
+        VarHolder.speed = MPH;
     }, 
 
     getMPH: function(){
