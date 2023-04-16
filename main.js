c990a486c701f1424cf8937345308788483cef3a
LeMemaker <derpyg61@gmail.com>
2023-04-16 00:19:21 -0400

    GUI fixes

--- main.js
+++ main.js
@@ -5,26 +5,32 @@ var disTrav = document.getElementById("distTrav");
 var carbEm = document.getElementById("carbEm");
 var time = document.getElementById("time");
 var toggle = document.getElementById("carToggle");
-
-
-function toggleDrive(){
-    if(this.carIsRunning){
-        this.carIsRunning = false;
-        this.tempMps = 0;
-        toggle.textContent = "Start Driving";
-    }
-    else{
-        this.carIsRunning = true;
-        this.tempMPS = 0.00833;
-        toggle.textContent = "Stop Driving";
-    }
-}
+var output = document.getElementById("output");
+var tree = document.getElementById("tree");
+var comSer = document.getElementById("comSer");
+var recycle = document.getElementById("recycle");
 
 
 var main =
 {
     carIsRunning: true,
     tempMPS:0.00833,
+    timeMov:1,
+    toggleDrive:function(){
+        if(main.carIsRunning == true){
+            main.carIsRunning = false;
+            main.tempMPS = 0;
+            main.timeMov = 0;
+            
+        }
+        else{
+            main.carIsRunning = true;
+            main.tempMPS = 0.00833;
+            main.timeMov = 1;
+            
+        }
+       
+    },
    
     getTotalMiles:function(){
         return carbonCalc.totalMiles;
@@ -46,7 +52,24 @@ var main =
         }
     },
     initializeMain: function(){
-        toggle.addEventListener("click", toggleDrive);
+        
+        toggle.addEventListener("click", main.toggleDrive);
+        
+        tree.addEventListener("click", function() {
+            var trees = main.getMilesToTrees()
+            output.textContent = trees;
+        });
+        
+        comSer.addEventListener("click", function() {
+            var community = main.getTrash();
+            output.textContent = community;
+        });
+        
+        recycle.addEventListener("click", function() {
+            var recycle = main.getrecycleCalculator();
+            output.textContent = recycle;
+        });
+
         main.inputCarType();
         var timeVal = 0;
         setInterval(function() {carbonCalc.totalMiles += main.tempMPS;
@@ -55,11 +78,11 @@ var main =
                                     disTrav.textContent = Math.round(carbonCalc.totalMiles*100)/100 + " miles";
                                     carbEm.textContent = Math.round((carbonCalc.CO2 / 1000) * 100) /100 + " kilograms";
                                     time.textContent = timeVal + " seconds";
-                                    timeVal += 1;
+                                    timeVal += main.timeMov;
 
                                 } , 1000);
             
-       
+     
     },
 
     getTotalCO2:function(){
@@ -67,15 +90,26 @@ var main =
     },
 
     getrecycleCalculator: function(){
-        return carbonCalc.recycleCalculator();
+        if(!main.carIsRunning){
+        toggle.addEventListener("click", main.to);
+            return carbonCalc.recycleCalculator();
+        }
+        else
+            alert("Stop driving to view details.");
     },
 
     getMilesToTrees: function(){
-        return carbonCalc.milesToTrees();
+        if(!main.carIsRunning)
+            return carbonCalc.milesToTrees();
+        else
+            alert("Stop driving to view details.");
     },
 
     getTrash: function(){
-        return carbonCalc.poundsCalculations();
+        if(!main.carIsRunning)
+            return carbonCalc.poundsCalculations();
+        else
+            alert("Stop driving to view details.");
     },
 
 
